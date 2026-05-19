cf8420819c250870c96e52021a343dd63b5b1876
AlvaroQQ <alvaro19aef@gmail.com>
2026-05-19 13:11:24 -0400

    Proyecto Spring Boot - Empresa Constructora - Configuracion inicial

---
+++ src/test/java/bo/constructora/backend/EmpresaConstructoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.constructora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaConstructoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
